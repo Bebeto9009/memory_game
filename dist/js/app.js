73f1b9a12cea4e39583f601f0982d3a7c7cf9035
bebeto9009 <adr.pawlak@gmail.com>
2018-10-10 00:29:39 +0200

    crud

--- dist/js/app.js
+++ dist/js/app.js
@@ -9,64 +9,6 @@ let gameRow = document.querySelector('.game__row');
 let elements = document.querySelectorAll('.fa-trash-alt');
 let elementsNum = elements.length;
 
-document.addEventListener('click', function(e){
-    if(e.path[0].className.includes('fa-trash-alt')) {
-        e.path[0].parentElement.remove();
-    }
-    if(e.path[0].className.includes('fa-check')) {
-        e.path[1].childNodes[1].disabled=true;
-    }
-    if(e.path[0].className.includes('fa-edit')) {
-        e.path[1].childNodes[1].disabled=false;
-    }
-})
-
-
-// document.getElementById('row').addEventListener('click', function(e) {
-//     if(e.target && e.target.className === 'fa-trash-alt') {
-//         console.log('udalo sie');
-//         console.log(e);
-//         // gameRow.parentNode.removeChild(gameRow);
-//     }
-// });
-
-// document.getElementById('row').addEventListener('click', function(e) {
-//     let classes = e.target.className;
-//     if(classes = ".fa-trash-alt"){
-//         gameRow.parentNode.removeChild(gameRow);
-//     }
-// })
-
-
-// document.getElementById("row").addEventListener("click", function(event) {
-//     if ( event.target.className === 'fa-trash-alt') {
-//         //Do your magic
-//         gameRow.parentNode.removeChild(gameRow);
-//     }
-// });
-
-
-// document.getElementById("row").addEventListener("click", function(event) {
-//     if ( event.target.className === 'fa-trash') {
-//         //Do your magic
-//         gameRow.parentNode.removeChild(gameRow);
-//     }
-// });
-
-// elements.forEach(element => {
-//     element.addEventListener('click', event => console.log(event));
-// })
-//
-// elements.forEach(function(element){
-//     element.addEventListener('click', function(event){
-//         if(element.target && element.target.id== 'brnPrepend')
-//     })
-// })
-//
-// trashIcon.addEventListener('click', function(event){
-//     console.log(event)
-// })
-
 //**************** fetch ****************//
 fetch(firstPlayer)
     .then(function (response) {
@@ -85,7 +27,7 @@ fetch(firstPlayer)
     })
     .catch(function (error) {
         console.log(error);
-    }); //fetch json first player
+    });
 //**************** fetch ****************//
 
 
@@ -105,6 +47,7 @@ function addNewRow() {
 
     let editPlayer = document.createElement('i');
     editPlayer.classList.add('fas', 'fa-edit');
+    editPlayer.style.display = 'none';
     // editPlayer.id = 'edit';
 
     let removePlayer = document.createElement('i');
@@ -119,94 +62,41 @@ function addNewRow() {
     playersRow.appendChild(noNewPlayer);
     playersRow.appendChild(namePlayer);
     playersRow.appendChild(editPlayer);
-    playersRow.appendChild(removePlayer);
     playersRow.appendChild(acceptNewPlayer);
+    playersRow.appendChild(removePlayer);
+
 }
 
 btnNewPlayer.addEventListener('click', function(event) {
     event.preventDefault();
     addNewRow();
-    // let el = document.querySelectorAll('.fa-trash-alt');
-    // let elNum = el.length;
-    // for (i = 0; i < elNum; i++){
-    //     console.log(el[i])
-    //     el[i].addEventListener('click', function(e) {
-    //         e.currentTarget.parentNode.remove();
-    //         console.log(e);
-    //     });
+}); // create row for new player
+
+document.addEventListener('click', function(e) {
+    if (e.target.className.includes('fa-trash-alt')) {
+        e.target.parentElement.remove();
+    }
+
+    // if (e.target.className.includes('fa-check')) {
+    //         e.target.parentNode.childNodes[1].disabled=true
+    //         e.target.style.display = 'none';
+    //         e.target.parentElement.childNodes[2].style.display = 'block';
     // }
-});
+    //
+    // if (e.target.className.includes('fa-edit')) {
+    //         e.target.parentNode.childNodes[1].disabled=false
+    //         e.target.style.display = 'none';
+    //         e.target.parentElement.childNodes[1].style.display = 'block';
+    // }
+        if (e.target.className.includes('fa-check')) {
+            e.target.parentNode.childNodes[1].disabled=true
+            e.target.parentElement.childNodes[2].style.display = 'block';
+            e.target.style.display = 'none';
 
+        } else if (e.target.className.includes('fa-edit')) {
+            e.target.parentNode.childNodes[1].disabled=false
+            e.target.parentElement.childNodes[1].style.display = 'block';
+            e.target.style.display = 'none';
+        }
 
-// function addPlayer () {
-//
-//     console.log('player został dodany ');
-// }
-
-// (function(){
-//     for (let i = 0; i < table.length; i++){
-//         let btnAdd = document.querySelectorAll('#accept'+table[i]);
-//     }
-// })();
-
-// /**************** add new player inputs ****************/
-// let count=1;
-// function newPlayer(){
-//     count++;
-//
-//     let playersRow = document.createElement('div');
-//     playersRow.classList.add('game__row');
-//     playersRow.id = count;
-//
-//     let noNewPlayer = document.createElement('input');
-//     noNewPlayer.classList.add('game-ordinar-numbers');
-//     noNewPlayer.disabled = true;
-//     noNewPlayer.value = count;
-//
-//     let namePlayer = document.createElement('input');
-//     namePlayer.classList.add('game-name');
-//
-//     let editPlayer = document.createElement('i');
-//     editPlayer.classList.add('fas', 'fa-edit');
-//
-//     let removePlayer = document.createElement('i');
-//     removePlayer.classList.add('fas', 'fa-trash-alt');
-//
-//     let acceptNewPlayer = document.createElement('i');
-//     acceptNewPlayer.classList.add('fas', 'fa-check');
-//
-//     list.appendChild(playersRow);
-//     playersRow.appendChild(noNewPlayer);
-//     playersRow.appendChild(namePlayer);
-//     playersRow.appendChild(editPlayer);
-//     playersRow.appendChild(removePlayer);
-//     playersRow.appendChild(acceptNewPlayer);
-// } // add new inputs for new player
-//
-// btnNewPlayer.addEventListener('click', function(event){
-//     event.preventDefault();
-//     newPlayer();
-// }); // create new player on button
-// /**************** add new player inputs ****************/
-//
-// /**************** delete player ****************/
-// function deletePlayer(){
-//     gameRow.parentNode.removeChild(gameRow);
-// }  // delete player function
-//
-// let deleteIcon = document.querySelector('.fa-trash-alt');
-// deleteIcon.addEventListener('click', deletePlayer);
-// // button to function delete player
-// /**************** delete player ****************/
-//
-// /**************** accept new player ****************/
-// let counter = [];
-// function addPlayer(){
-//     counter.push(namePlayer.value); // wrzucamy do tablicy wartość name
-//     gameRow.id = counter.indexOf(namePlayer.value); // dodajemy id z kolejności elementu nazwy gracza w tablicy
-// }
-//
-// let acceptPlayer = document.getElementById(counter.indexOf(namePlayer.value));
-// console.log(acceptPlayer)
-// acceptPlayer.addEventListener('click', addPlayer);
-// /**************** accept new player ****************/
+}); //crud
