74a521260ba64286e88b9489e78d714c03bebde5
Adrian <adrian@netkata.com>
2018-11-14 15:58:44 +0100

    add new character

--- js/game.js
+++ js/game.js
@@ -1,32 +1,88 @@
+const startBtn = document.querySelector('.btn-play');
+const memoryGame = document.getElementById('memory__game');
 const memory = {
+    gameList : document.getElementById('game'),
     randomCards: [],
     checkedCards: [],
     click: 0,
     img: [{
-        'name' : 'koopa_troopa',
-        'img' : 'img/koopa_troopa.jpg',
-    },
-    {
-        'name' : 'luigi',
-        'img' : 'img/luigi.jpg',
-    },
-    {
-        'name' : 'mario',
-        'img' : 'img/mario.jpg',
-    },
-    {
-        'name' : 'mushroom',
-        'img' : 'img/mushroom.jpg',
-    },
-    {
-        'name' : 'yoshi',
-        'img' : 'img/yoshi.jpg',
-    }
+            'name' : 'koopa_troopa',
+            'img' : 'img/koopa_troopa.jpg',
+        },
+        {
+            'name' : 'luigi',
+            'img' : 'img/luigi.jpg',
+        },
+        {
+            'name' : 'mario',
+            'img' : 'img/mario.jpg',
+        },
+        {
+            'name' : 'mushroom',
+            'img' : 'img/mushroom.jpg',
+        },
+        {
+            'name' : 'yoshi',
+            'img' : 'img/yoshi.jpg',
+        },
+        {
+            'name' : 'boo',
+            'img' : 'img/boo.jpg',
+        },
+        {
+            'name' : 'bowser',
+            'img' : 'img/bowser.jpg',
+        },
+        {
+            'name' : 'donkey_kong',
+            'img' : 'img/donkey_kong.jpg',
+        },
+        {
+            'name' : 'goomba',
+            'img' : 'img/goomba.jpg',
+        },
+        {
+            'name' : 'kamek',
+            'img' : 'img/kamek.jpg',
+        },
+        {
+            'name' : 'peach',
+            'img' : 'img/peach.jpg',
+        },
+        {
+            'name' : 'pika',
+            'img' : 'img/pika.jpg',
+        },
+        {
+            'name' : 'plant',
+            'img' : 'img/plant.jpg',
+        },
+        {
+            'name' : 'star',
+            'img' : 'img/star.jpg',
+        },
+        {
+            'name' : 'toad',
+            'img' : 'img/toad.jpg',
+        },
+        {
+            'name' : 'toadette',
+            'img' : 'img/toadette.jpg',
+        },
+        {
+            'name' : 'waluigi',
+            'img' : 'img/waluigi.jpg',
+        },
+        {
+            'name' : 'wario',
+            'img' : 'img/wario.jpg',
+        }
     ],
 
     startGame : function() {
+        this.gameList.style.display = 'none';
         const shuffled = this.img.sort(() => .5 - Math.random()); // shuffle
-        let selected = shuffled.slice(0,2) ; //get 2 first elements from shuffle
+        let selected = shuffled.slice(0,5) ; //get 2 first elements from shuffle
         this.randomCards.push(selected);
         let allCard = this.randomCards[0].concat(this.randomCards[0]);
         allCard.sort(() => .5 - Math.random());
@@ -73,5 +129,10 @@ const memory = {
         }
     }
 } // end object memory
-
-memory.startGame();
+// startBtn.addEventListener('click', memory.startGame);
+startBtn.addEventListener('click', event => {
+    event.preventDefault();
+    memoryGame.style.height = '100vh';
+    memory.startGame();
+})
+// memory.startGame();
