878cc5c1acc7b503ff052a8715582a9051a41f04
Adrian <adrian@netkata.com>
2018-12-14 15:51:56 +0100

    bot

--- dist/js/bot.js
+++ dist/js/bot.js
@@ -1,4 +1,40 @@
 function test() {
-    document.querySelectorAll('.memory-game__item');
-    for (let i = 0; )
+    let arr = [];
+    let el = document.querySelectorAll('.memory-game__item').dataset.name;
+    for (let i = 0; i < el.length; i++ ) {
+        el.click();
+    }
 };
+
+
+// selectCard : function(e) {
+//     if (this.canPlay) {
+//         e.target.classList.add('memory-game__item--select');
+//         e.target.classList.add('memory-game__item--disabled');
+//
+//         setTimeout(function () {
+//             e.target.classList.remove('memory-game__item--reverse');
+//         }, 250);
+//
+//         if (this.click < 3) {
+//             if (this.firstchoice.length === 0) {
+//                 this.firstchoice = e.target;
+//
+//             } else if (this.firstchoice !== e.target) {
+//                 this.secondchoice = e.target;
+//                 this.canPlay = false;
+//                 this.click++;
+//
+//                 if (this.firstchoice.dataset.name === this.secondchoice.dataset.name) {
+//                     setTimeout(() => {
+//                         this.matchCard();
+//                     }, 500);
+//                 } else {
+//                     setTimeout(() => {
+//                         this.mismatchCard();
+//                     }, 500);
+//                 }
+//             }
+//         }
+//     }
+// },
